9e098b16ebc600257bd77c88cc74506a0183781b
Guimartinssilva <guimartinssilva23@gmail.com>
2024-10-28 20:55:14 -0300

    Aulas de Operadores

---
+++ Operadores/Script1.js
@@ -0,0 +1,37 @@
+// let valor1 = 3
+// let valor2 = 4
+
+// resultado = valor1 + valor2
+
+// console.log(resultado)
+
+// let valor3 = 3
+// let valor4 = 5
+// let valor5 = 2
+
+// resultado1 = (valor3 * valor4) / valor5
+
+// console.log(resultado1)
+
+// let valor6 = 5
+// let valor7 = 4
+// let valor8 = -1
+
+// resultado2 = (valor6 - valor7) * valor8
+
+// console.log(resultado2)
+
+// let valor9 = 234
+// let valor10 = 5
+
+// resultado3 = valor9 % valor10
+
+// console.log(resultado3)
+
+let valor11 = 23
+let valor12 = 15
+
+console.log(valor11===valor12)
+console.log(valor11!==valor12)
+console.log(valor11>valor12)
+console.log(valor11<valor12)
